6df04ea905d788c73c8f6040293c8da95170cc33
@hexlet/cli <support@hexlet.io>
2023-01-18 23:55:14 +0300

    submit ruby-basics-ru/collections

--- ruby-basics-ru/collections/lib/compare_versions.rb
+++ ruby-basics-ru/collections/lib/compare_versions.rb
@@ -2,17 +2,9 @@
 
 # BEGIN
 def compare_versions(ver1, ver2)
-  maj1, min1 = ver1.split('.').map(&:to_i)
-  maj2, min2 = ver2.split('.').map(&:to_i)
+  v1 = ver1.split('.').map(&:to_i)
+  v2 = ver2.split('.').map(&:to_i)
 
-  return 1 if maj1 > maj2
-
-  return -1 if maj1 < maj2
-
-  return 1 if min1 > min2
-
-  return -1 if min1 < min2
-
-  0
+  v1 <=> 2
 end
 # END
