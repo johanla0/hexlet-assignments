b1940779a60e158541a775c5c37ca61241b115c2
@hexlet/cli <support@hexlet.io>
2023-01-19 23:20:11 +0300

    submit ruby-basics-ru/basics

--- ruby-basics-ru/basics/lib/fizz_buzz.rb
+++ ruby-basics-ru/basics/lib/fizz_buzz.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
 # BEGIN
+class Integer
+  def fizz_buzz
+    return_word_if_divisible = ->(n, w, num) { (num % n).zero? ? w : '' }
+
+    fizz = return_word_if_divisible.curry.call(3, 'Fizz')
+    buzz = return_word_if_divisible.curry.call(5, 'Buzz')
+
+    result = ''
+    result += fizz.call(self)
+    result += buzz.call(self)
+    result.empty? ? self : result
+  end
+end
+
 def fizz_buzz(start, stop)
   return nil if start > stop
 
-  (start..stop).to_a.map do |n|
-    next n.to_s unless fiz?(n) || buz?(n)
-
-    result = ''
-    result += 'Fizz' if fiz?(n)
-    result += 'Buzz' if buz?(n)
-    result
-  end.join(' ')
-end
-
-def fiz?(number)
-  (number % 3).zero?
-end
-
-def buz?(number)
-  (number % 5).zero?
+  (start..stop).to_a.map(&:fizz_buzz).join(' ')
 end
 # END
