67c8eb03663a79017ca040cabe4cf396b6ab64ff
@hexlet/cli <support@hexlet.io>
2023-06-03 11:46:41 +0300

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -2,7 +2,13 @@
 
 Rails.application.routes.draw do
   # BEGIN
-  
+  root 'home#index'
+  scope '/:locale', locale: /en|ru/ do
+    resources :posts
+  end
+  scope module: :posts do
+    resources :post_comments
+  end
   # END
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- rails-basics-ru/i18n/db/seeds.rb
+++ rails-basics-ru/i18n/db/seeds.rb
@@ -6,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Post.create(
+    title: Faker::Lorem.word,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+5.times do
+  Post::Comment.create(
+    body: Faker::Lorem.paragraph,
+    post: Post.all.sample
+  )
+end
