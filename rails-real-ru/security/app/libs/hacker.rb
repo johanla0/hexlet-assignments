0f5bbbea71a8084351b59c1d13e209019d2ce5cd
@hexlet/cli <support@hexlet.io>
2023-06-28 09:05:38 +0300

    submit rails-real-ru/security

--- rails-real-ru/security/app/libs/hacker.rb
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -3,26 +3,27 @@
 require 'nokogiri'
 require 'open-uri'
 require 'net/http'
-require 'net/https'
+
+WebMock.allow_net_connect! if Rails.env.test?
 
 class Hacker
-  ROOT_PATH = 'https://rails-collective-blog-ru.hexlet.app'
-  GET_PATH = '/users/sign_up'
-  POST_PATH = '/users'
-
   class << self
     def hack(email, password)
-      uri = URI.parse(ROOT_PATH)
+      hostname = 'https://rails-collective-blog-ru.hexlet.app'
+      uri = URI.parse(hostname)
+      get_path = '/users/sign_up'
+      post_path = '/users'
 
       Net::HTTP.start(uri.host,
                       uri.port,
                       use_ssl: uri.scheme == 'https',
                       verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-        request = Net::HTTP::Get.new uri.request_uri + GET_PATH
+        request = Net::HTTP::Get.new uri.request_uri + get_path
         response = http.request request
 
+
         doc = Nokogiri::HTML(response.body)
-        authenticity_token = doc.at('meta[name="csrf-token"]')['content']
+        authenticity_token = doc.at('#new_user > input[type=hidden]')['value']
 
         cookie = response.response['set-cookie'].split('; ')[0]
 
@@ -34,13 +35,12 @@ class Hacker
 
         params = { authenticity_token:, user: }
 
-        request = Net::HTTP::Post.new uri.request_uri + POST_PATH
+        request = Net::HTTP::Post.new uri.request_uri + post_path
         request.body = URI.encode_www_form(params)
         request['Cookie'] = cookie
 
-        response = http.request request
 
-        # puts response.code
+        http.request request
       end
     end
   end
