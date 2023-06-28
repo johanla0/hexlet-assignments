ce95646321114fb318c315b9d4e7965db72b8bb5
@hexlet/cli <support@hexlet.io>
2023-06-28 09:18:12 +0300

    submit rails-real-ru/security

--- rails-real-ru/security/app/libs/hacker.rb
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -4,44 +4,38 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/http'
 
-WebMock.allow_net_connect! if Rails.env.test?
-
 class Hacker
   class << self
     def hack(email, password)
-      hostname = 'https://rails-collective-blog-ru.hexlet.app'
+      hostname = 'https://rails-collective-blog-ru.hexlet.app/'
+      get_path = 'users/sign_up'
+      post_path = 'users'
+
       uri = URI.parse(hostname)
-      get_path = '/users/sign_up'
-      post_path = '/users'
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == 'https'
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      Net::HTTP.start(uri.host,
-                      uri.port,
-                      use_ssl: uri.scheme == 'https',
-                      verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-        request = Net::HTTP::Get.new uri.request_uri + get_path
-        response = http.request request
+      request = Net::HTTP::Get.new uri.request_uri + get_path
+      response = http.request request
 
+      doc = Nokogiri::HTML(response.body)
+      authenticity_token = doc.at('input[name="authenticity_token"]')['value']
 
-        doc = Nokogiri::HTML(response.body)
-        authenticity_token = doc.at('#new_user > input[type=hidden]')['value']
+      cookie = response.response['set-cookie'].split('; ')[0]
 
-        cookie = response.response['set-cookie'].split('; ')[0]
+      params = {
+        'user[email]': email,
+        'user[password]': password,
+        'user[password_confirmation]': password,
+        authenticity_token:
+      }
 
-        user = {
-          email:,
-          password:,
-          password_confirmation: password
-        }
+      request = Net::HTTP::Post.new uri.request_uri + post_path
+      request.body = URI.encode_www_form(params)
+      request['Cookie'] = cookie
 
-        params = { authenticity_token:, user: }
-
-        request = Net::HTTP::Post.new uri.request_uri + post_path
-        request.body = URI.encode_www_form(params)
-        request['Cookie'] = cookie
-
-
-        http.request request
-      end
+      http.request request
     end
   end
 end
