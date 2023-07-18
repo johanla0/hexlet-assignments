3f162fb2268a637c1627ecc3e10df4df871e9153
Ivan Dolgov <idlhero@gmail.com>
2023-07-18 13:01:23 +0300

    Refactor

--- rails-real-ru/emails/config/environments/production.rb
+++ rails-real-ru/emails/config/environments/production.rb
@@ -62,14 +62,14 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # BEGIN
-  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST', nil) }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name: ENV['MAIL_USERNAME'],
-    password: ENV['MAIL_PASSWORD'],
-    address: ENV['MAIL_HOST'],
-    domain: ENV['MAIL_HOST'],
-    port: ENV['SMTP_PORT'] || '25',
+    user_name: ENV.fetch('MAIL_USERNAME', nil),
+    password: ENV.fetch('MAIL_PASSWORD', nil),
+    address: ENV.fetch('MAIL_HOST', nil),
+    domain: ENV.fetch('MAIL_HOST', nil),
+    port: ENV.fetch('SMTP_PORT', '25'),
     authentication: :cram_md5
   }
   # END
